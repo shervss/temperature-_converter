f2602762bd6a9ebd7f0fc910b49973031ea2860e
Shervin Mangulabnan <163648798+shervss@users.noreply.github.com>
2024-09-30 12:35:29 +0800

    add pseudocode

---
+++ temperature_converter.py
@@ -0,0 +1,7 @@
+# Program by: Shervin Marco Mangulabnan
+# Date of Creation: September 26, 2024
+# Purpose: a program that converts temperatures between Celsius and Fahrenheit
+
+# Ask the user to input a temperature
+# Ask the user for the conversion type
+# Perform the appropriate conversion
